e619b9542e3508400d1c9446141ffe09b7fafdbf
shahadathhs <shahadathhossensajib732@gmail.com>
2024-12-20 09:36:36 +0600

    feat: improved api logger middleware

--- src/app/middlewares/apiInfoLogger.ts
+++ src/app/middlewares/apiInfoLogger.ts
@@ -1,18 +1,61 @@
 import { Request, Response, NextFunction } from 'express'
 
+import { configuration } from '../config/configuration'
 import { logger } from '../log/logger'
 
 const apiInfoLogger = (req: Request, res: Response, next: NextFunction) => {
-  const logDetails = `
-    Method: ${req.method}
-    URL: ${req.url}
-    Body: ${req.body && Object.keys(req.body).length > 0 ? JSON.stringify(req.body) : 'No body content'}
-    Query: ${req.query && Object.keys(req.query).length > 0 ? JSON.stringify(req.query) : 'No query content'}
-    Params: ${req.params && Object.keys(req.params).length > 0 ? JSON.stringify(req.params) : 'No params content'}
-    Headers: ${JSON.stringify(req.headers)}
-  `
+  // * Log the incoming request details
+  const logDetails = {
+    method: req.method,
+    url: req.url,
+    body:
+      req.body && Object.keys(req.body).length > 0
+        ? JSON.stringify(req.body)
+        : 'N/A',
+    query:
+      req.query && Object.keys(req.query).length > 0
+        ? JSON.stringify(req.query)
+        : 'N/A',
+    params:
+      req.params && Object.keys(req.params).length > 0
+        ? JSON.stringify(req.params)
+        : 'N/A',
+    token:
+      configuration.env === 'development'
+        ? req.headers.authorization || 'N/A'
+        : null,
+    cookies: configuration.env === 'development' ? req.cookies || 'N/A' : null
+  }
 
-  logger.info(`Incoming Request: ${logDetails.trim()}`)
+  logger.info(`Incoming Request: ${JSON.stringify(logDetails)}`)
+
+  // * Capture response body
+  let responseBody: unknown = null
+  const originalSend = res.send
+  const originalJson = res.json
+
+  res.send = function (body: unknown): Response {
+    responseBody = body
+    return originalSend.call(this, body)
+  }
+
+  res.json = function (body: unknown): Response {
+    responseBody = body
+    return originalJson.call(this, body)
+  }
+
+  // * Log response details after the response is sent
+  res.on('finish', () => {
+    const responseLog = {
+      statusCode: res.statusCode,
+      responseBody: responseBody
+        ? JSON.stringify(responseBody)
+        : 'No response body'
+    }
+
+    logger.info(`Response Sent: ${JSON.stringify(responseLog)}`)
+  })
+
   next()
 }
 

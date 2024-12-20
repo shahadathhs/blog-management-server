49bc94fbe217e2721e02d8194d6348658423c8b7
shahadathhs <shahadathhossensajib732@gmail.com>
2024-12-20 17:19:32 +0600

    feat: made sendResponse utils

---
+++ src/app/utils/sendResponse.ts
@@ -0,0 +1,25 @@
+import { Response } from 'express';
+
+interface IResponse {
+  statusCode: number;
+  success: boolean;
+  message: string;
+  data?: object | Array<object> | null;
+}
+
+const sendResponse = (res: Response, data: IResponse) => {
+  const response: IResponse = {
+    statusCode: data.statusCode,
+    success: data.success,
+    message: data.message,
+  };
+
+  // * Add the `data` key only if it is defined
+  if (data.data !== undefined) {
+    response.data = data.data;
+  }
+
+  res.status(data.statusCode).json(response);
+};
+
+export default sendResponse;
